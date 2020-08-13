3d2b36b9dea04a4ee9e4101ec0f98d567d131de2
Tshilidzi Mphelo <temphelo95@gmail.com>
2020-08-13 21:22:14 +0200

    Created the Student class and its instance variables

---
+++ StudentInfo_system/StudentEntities/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfo_system.StudentEntities
+{
+    #region
+    public class Student
+    //2.1. This is to make its methods and variables accessible to all classes even if they are not in the same folder
+    {
+        //2.2.2. I'm giving them private access modifiers so they cannot be easily accessible to outside class, so their values aren't changed
+        private int studentID;
+        private char gender;
+        private string populationGroup;
+        private string homeLanguage;
+        private char saCitizenshipStat;
+        private string foreignCountry;
+    }
+}
